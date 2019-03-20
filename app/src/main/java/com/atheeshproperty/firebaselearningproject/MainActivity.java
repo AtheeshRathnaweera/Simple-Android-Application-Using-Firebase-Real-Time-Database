8688d142538310289293de4b5249299de04202c5
AtheeshRathnaweera27 <rathnaweeraatheesh72@gmail.com>
2019-03-21 02:05:05 +0530

    Added the connection checking methods.

--- app/src/main/java/com/atheeshproperty/firebaselearningproject/MainActivity.java
+++ app/src/main/java/com/atheeshproperty/firebaselearningproject/MainActivity.java
@@ -1,14 +1,21 @@
 package com.atheeshproperty.firebaselearningproject;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Random;
 
@@ -37,7 +44,18 @@ public class MainActivity extends AppCompatActivity {
         phoneIn = findViewById(R.id.phone);
 
         SaveButton();
+        viewButton();
 
+    }
+
+    private void viewButton(){
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewDataInDatabase();
+            }
+        });
     }
 
     private void SaveButton(){
@@ -45,22 +63,28 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                int bound = 10000;
+                if(checkDataStatus()){
+                    int bound = 10000;
 
-                Random rand = new Random();
-                int randomNumber = rand.nextInt(bound);
-                String random = String.valueOf(randomNumber);
+                    Random rand = new Random();
+                    int randomNumber = rand.nextInt(bound);
+                    String random = String.valueOf(randomNumber);
 
-                String firstName = first.getText().toString();
-                String lastName = last.getText().toString();
-                String age = ageIn.getText().toString();
-                String phoneNu = phoneIn.getText().toString();
+                    String firstName = first.getText().toString();
+                    String lastName = last.getText().toString();
+                    String age = ageIn.getText().toString();
+                    String phoneNu = phoneIn.getText().toString();
 
-                UserProfileData user = new UserProfileData(firstName, lastName,age,phoneNu);
+                    UserProfileData user = new UserProfileData(firstName, lastName,age,phoneNu);
 
-                myRef.child("users").child(random).setValue(user);
+                    myRef.child("users").push().setValue(user);
 
-                clearTheFields();
+                    clearTheFields();
+                }else{
+                    //Nothing will happen
+                }
+
+
 
             }
         });
@@ -73,4 +97,50 @@ public class MainActivity extends AppCompatActivity {
         ageIn.setText("");
         phoneIn.setText("");
     }
+
+
+
+    public boolean checkDataStatus() {
+        final ConnectivityManager connMgr = (ConnectivityManager)
+                this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        final android.net.NetworkInfo wifi = connMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        final android.net.NetworkInfo mobile = connMgr.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        if (wifi.isConnectedOrConnecting ()) {
+            Log.e("Network","Wifi");
+            return true;
+        } else if (mobile.isConnectedOrConnecting ()) {
+            Log.e("Network","Mobile data");
+            return true;
+        } else {
+            Toast.makeText(this, "Please check your network connection! ", Toast.LENGTH_LONG).show();
+            return false;
+        }
+    }
+
+    public void viewDataInDatabase(){
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                if(dataSnapshot.exists()){
+                    Log.e("Date exists","True");
+                    UserProfileData post = dataSnapshot.getValue(UserProfileData.class);
+                    Log.e("Date exists","Data"+post.toString());
+            }
+                Log.e("Date exists","False");
+                // ...
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.e("Error occured",databaseError.toException().toString());
+                // ...
+            }
+        };
+        myRef.addValueEventListener(postListener);
+
+    }
+
+
 }
